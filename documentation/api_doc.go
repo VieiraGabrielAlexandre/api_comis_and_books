74fa4d76a5d4f3cad2d3278d5230a0d63bc34e6e
Gabriel Vieira <gabriel.vieira@origoeneria.com.br>
2023-10-26 18:42:32 -0300

    swagger implementation

---
+++ documentation/api_doc.go
@@ -0,0 +1,95 @@
+package documentation
+
+import (
+	"net/http"
+)
+
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/// ErrorResponse represents the error response.
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+// @Summary Create a new comic
+// @Description Create a new comic entry
+// @Accept json
+// @Produce json
+// @Param comic body Comics true "Comic object"
+// @Success 201 {string} string "Comic created successfully"
+// @Router /comics [post]
+func Create(w http.ResponseWriter, r *http.Request) {
+	// Your existing code here
+}
+
+// @Summary Get a comic by ID
+// @Description Get a comic by its ID
+// @Produce json
+// @Param id path int true "Comic ID"
+// @Success 200 {object} Comics "Comic object"
+// @Failure 500 {object} ErrorResponse "Error response"
+// @Router /comics/{id} [get]
+func GetByID(w http.ResponseWriter, r *http.Request) {
+	// Your existing code here
+}
+
+// @Summary Update a comic by ID
+// @Description Update a comic by its ID
+// @Accept json
+// @Produce json
+// @Param id path int true "Comic ID"
+// @Param comic body Comics true "Updated comic object"
+// @Success 200 {string} string "Comic updated successfully"
+// @Router /comics/{id} [put]
+func Update(w http.ResponseWriter, r *http.Request) {
+	// Your existing code here
+}
+
+// @Summary Get comics with pagination
+// @Description Get a list of comics with pagination
+// @Produce json
+// @Param page query int false "Page number"
+// @Param limit query int false "Results per page"
+// @Param orderBy query string false "Order by"
+// @Param filter query string false "Filter"
+// @Param column query string false "Column"
+// @Param order query string false "Order"
+// @Success 200 {object} Comics "Comics response"
+// @Failure 500 {object} ErrorResponse "Error response"
+// @Router /comics [get]
+func GetAllPaginate(w http.ResponseWriter, r *http.Request) {
+	// Your existing code here
+}
+
+// @Summary Delete a comic by ID
+// @Description Delete a comic by its ID
+// @Produce json
+// @Param id path int true "Comic ID"
+// @Success 200 {string} string "Comic deleted successfully"
+// @Router /comics/{id} [delete]
+func Delete(w http.ResponseWriter, r *http.Request) {
+	// Your existing code here
+}
+
+type Comics struct {
+	Title     string `json:"title"`
+	Volume    int    `json:"volume"`
+	Publisher string `json:"publisher"`
+	Editor    string `json:"editor"`
+}
--- go.mod
+++ go.mod
@@ -5,12 +5,28 @@ go 1.18
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.5.1
+	github.com/swaggo/http-swagger/example/go-chi v0.0.0-20230830153024-537f045bded0
+	github.com/swaggo/swag v1.16.2
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.20.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/http-swagger v1.3.4 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
